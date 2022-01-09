594b17d7a3f7f894c44e9808abc976e415053362
Sergio Castillo <sergicaru@gmail.com>
2022-01-09 19:46:43 +0100

    add navigation from home to detail

---
+++ app/src/main/java/com/example/ems/view/ActivePowerChartsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.ems.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivePowerChartsScreen() {
+  Text(text = "Detail")
+}
---
+++ app/src/main/java/com/example/ems/view/EnergyLevelScreen.kt
@@ -0,0 +1,45 @@
+package com.example.ems.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.example.ems.repository.ActivePowerDTO
+import com.example.ems.repository.EnergyDTO
+
+
+@Composable
+fun EnergyLevelScreen(
+  navController: NavHostController,
+  quasarEnergyDischarged: Float,
+  quasarEnergyCharged: Float,
+  energyData: EnergyDTO? = null,
+  activePower: List<ActivePowerDTO>? = null
+) {
+  Column() {
+    Text(text = "Discharged -> $quasarEnergyDischarged kWh")
+    Text(text = "Charged -> $quasarEnergyCharged kWh")
+
+    if (energyData != null) {
+      LiveDataWidget(energyData)
+    }
+    if (activePower != null) {
+      Button(onClick = { navController.navigate("detail") }) {
+        Text(text = "Go to detail")
+      }
+    }
+  }
+}
+
+@Composable
+fun LiveDataWidget(data: EnergyDTO) {
+  Text(text = "Live Data")
+  Text(text = "solar_power -> ${data.solarPower}")
+  Text(text = "quasars_power -> ${data.quasarsPower}")
+  Text(text = "grid_power -> ${data.gridPower}")
+  Text(text = "building_demand -> ${data.buildingDemand}")
+  Text(text = "system_soc -> ${data.systemSoc}")
+  Text(text = "total_energy -> ${data.totalEnergy}")
+  Text(text = "current_energy-> ${data.currentEnergy}")
+}
